014dc7d5f8ec4c9adab27b541680ebb22017858c
ktnlvr <artur.roos.artur@gmail.com>
2025-03-27 01:09:31 +0200

    Do a basic neovim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,17 @@
-require "options"
-require "plug"
+require('core')
+
+-- Yaaay plugins
+require "paq" {
+    "savq/paq-nvim",
+    "neovim/nvim-lspconfig", 
+
+    { 'nvim-lua/plenary.nvim' }, -- Dependency for telescope
+    { 'nvim-telescope/telescope.nvim' },
+
+    { "lervag/vimtex", opt = true },
+
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'nvim-treesitter/playground' },
+}
+
+require('remap')
