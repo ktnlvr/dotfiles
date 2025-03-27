dea5cff93958be54cdecfed3453c1debb3e71e3e
Artur Roos <artur.roos.artur@gmail.com>
2025-03-27 12:31:01 +0200

    Add some more neovim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,10 +5,12 @@ require "paq" {
     "savq/paq-nvim",
     "neovim/nvim-lspconfig", 
 
-    { 'nvim-lua/plenary.nvim' }, -- Dependency for telescope
-    { 'nvim-telescope/telescope.nvim' },
+    'nvim-lua/plenary.nvim', -- Dependency for telescope, hop and neogit
+    'nvim-telescope/telescope.nvim',
 
     { "lervag/vimtex", opt = true },
+    { "phaazon/hop.nvim", branch = "v2" },
+    "NeogitOrg/neogit",
 
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
     { 'nvim-treesitter/playground' },
